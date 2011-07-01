efe46a82cd4821dc7518ffe21acd233bed426108
William Cheung <wcheung@irise.com>
2011-07-01 16:11:57 -0400

    Make main method return void (Unit) so java finds main.

--- src/ScalaHadoopTool.scala
+++ src/ScalaHadoopTool.scala
@@ -2,7 +2,7 @@ package com.asimma.ScalaHadoop;
 
 abstract class ScalaHadoopTool extends org.apache.hadoop.conf.Configured with org.apache.hadoop.util.Tool  {
 
-  def main(args: Array[String]) = {
+  def main(args: Array[String]) : Unit = {
     org.apache.hadoop.util.ToolRunner.run(this, args);
   }
 }
